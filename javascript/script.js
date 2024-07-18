97f39587084b64dbf8618e3cad0de2208764cf73
DannyFabs <aderemifawoyed@gmail.com>
2024-07-18 10:35:33 -0500

    Updated home page and links

--- javascript/script.js
+++ javascript/script.js
@@ -2,9 +2,20 @@
 
 const hamburgerIcon = document.querySelector('.icon');
 const offScreenMenu = document.querySelector('.menu');
+
+const pageLinks = document.querySelectorAll('.menu--item');
+
 const closeIcon = document.querySelector('.close')
 const close = document.getElementById("close")
 
+pageLinks.forEach(link => {
+    link.addEventListener('click', function() {
+        pageLinks.forEach(link => link.classList.remove('active'));
+        this.classList.toggle('active')
+        offScreenMenu.classList.remove('active')
+        close.style.display="none"
+    })
+});
 
 hamburgerIcon.addEventListener("click",function() {
     close.style.display="inline"
